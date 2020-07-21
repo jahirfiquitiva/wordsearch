2f96e438d1d3e2f3361606931d22a062cc4c1ce3
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-07-21 15:26:17 -0500

    Add table

---
+++ components/sopa/sopa-letras.js
@@ -0,0 +1,45 @@
+import styles from './sopa.module.css';
+
+const SopaLetras = () => {
+  return (
+    <table className={styles.sopa}>
+      <tr>
+        <td>a</td>
+        <td>a</td>
+        <td>a</td>
+        <td>a</td>
+        <td>a</td>
+      </tr>
+      <tr>
+        <td>a</td>
+        <td>a</td>
+        <td>a</td>
+        <td>a</td>
+        <td>a</td>
+      </tr>
+      <tr>
+        <td>a</td>
+        <td>a</td>
+        <td>a</td>
+        <td>a</td>
+        <td>a</td>
+      </tr>
+      <tr>
+        <td>a</td>
+        <td>a</td>
+        <td>a</td>
+        <td>a</td>
+        <td>a</td>
+      </tr>
+      <tr>
+        <td>a</td>
+        <td>a</td>
+        <td>a</td>
+        <td>a</td>
+        <td>a</td>
+      </tr>
+    </table>
+  );
+};
+
+export default SopaLetras;
