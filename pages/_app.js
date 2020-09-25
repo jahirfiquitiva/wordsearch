2fc9faf391976ae0fb747512ca27d8ef1e09a57f
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-09-25 18:53:51 -0500

    Add reset and normalize css. Improve default styles

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '@styles/resetalize.min.css';
+import '@styles/global.css';
+
+const MyApp = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
