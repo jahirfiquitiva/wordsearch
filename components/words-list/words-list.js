b9606df581bd60ca1a6cb861740a870e46ebc671
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-09-25 18:34:50 -0500

    More progress on refactor and splitting components

---
+++ components/words-list/words-list.js
@@ -0,0 +1,15 @@
+import { useContext } from 'react';
+import { WordSearchContext } from '@components/ws/ws-wrapper';
+import styles from './words-list.module.css';
+
+const WordsList = () => {
+  const { placed, solvedWords } = useContext(WordSearchContext);
+
+  return (<ul className={styles.words}>
+    {(Object.keys(placed) || []).map((it, i) => {
+      return (<li key={i} className={solvedWords.includes(it) ? styles.solved : ''}>{it}</li>);
+    })}
+  </ul>);
+};
+
+export default WordsList;
