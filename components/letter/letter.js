2a6e2017f9cdb4b9bca4e9210a982fac756d722f
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-09-25 18:31:33 -0500

    Progress refactoring project. Split wordsearch components into smaller ones

---
+++ components/letter/letter.js
@@ -0,0 +1,16 @@
+import { useContext } from 'react';
+import { WordSearchContext } from '@components/ws/ws-wrapper';
+import styles from './letter.module.css';
+
+const Letter = ({ letter, x, y, onClick }) => {
+  const { selectedLetters } = useContext(WordSearchContext);
+  const active = (selectedLetters || []).includes(`${x},${y}`);
+
+  return (<td
+    className={`${styles.letter} ${active ? styles.active : ''} ${letter === '-' ? styles.cw : ''}`}
+    onClick={onClick}>
+    {letter}
+  </td>);
+};
+
+export default Letter;
---
+++ components/table/table.js
@@ -0,0 +1,38 @@
+import { useContext } from 'react';
+import { WordSearchContext } from '@components/ws/ws-wrapper';
+import Letter from '@components/letter/letter';
+import styles from './table.module.css';
+
+const Table = () => {
+  const { puzzle, selectedLetters, setSelectedLetters } = useContext(WordSearchContext);
+
+  const toggleLetter = (letter, x, y) => {
+    if (letter === '-') return;
+    const key = `${x},${y}`;
+    setSelectedLetters({
+      type: (selectedLetters || []).includes(key) ? 'remove' : 'add',
+      payload: key,
+    });
+  };
+
+  return (<table className={styles.ws}>
+    <tbody>
+      {(puzzle || []).map((row, i) => {
+        return (
+          <tr key={i}>
+            {(row || []).map((it, j) => {
+              return (
+                <Letter
+                  x={j} y={i}
+                  key={j} letter={it}
+                  onClick={() => { toggleLetter(it, j, i); }}/>
+              );
+            })}
+          </tr>
+        );
+      })}
+    </tbody>
+  </table>);
+};
+
+export default Table;
