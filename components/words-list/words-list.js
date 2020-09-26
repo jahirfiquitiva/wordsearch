028d6307bb600367a63b9b8ca30f7662fee97811
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-09-25 19:05:53 -0500

    Improve overall design

--- components/words-list/words-list.js
+++ components/words-list/words-list.js
@@ -5,11 +5,14 @@ import styles from './words-list.module.css';
 const WordsList = () => {
   const { placed, solvedWords } = useContext(WordSearchContext);
 
-  return (<ul className={styles.words}>
-    {(Object.keys(placed) || []).map((it, i) => {
-      return (<li key={i} className={solvedWords.includes(it) ? styles.solved : ''}>{it}</li>);
-    })}
-  </ul>);
+  return (<div>
+    <p><b>Try to find the following words:</b></p>
+    <ul className={styles.words}>
+      {(Object.keys(placed) || []).map((it, i) => {
+        return (<li key={i} className={solvedWords.includes(it) ? styles.solved : ''}>{it}</li>);
+      })}
+    </ul>
+  </div>);
 };
 
 export default WordsList;
